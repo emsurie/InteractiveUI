f6ffe1214b1de8d1802d1c67ea2fca32d72e7bfa
Emily Suriel <emilysuriel0@gmail.com>
2025-07-09 13:15:07 -0400

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Indiana Suriel on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
